99b1be6623196c13921560943f2387301fba77fc
FelixSU <felixsummerer@freenet.de>
2024-12-23 10:20:52 +0100

    declare more modules

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -2,3 +2,12 @@ declare module "*.png";
 declare module "*.svg";
 declare module "*.jpeg";
 declare module "*.jpg";
+
+declare module 'react-multi-date-picker';
+declare module 'react-multi-date-picker/plugins/time_picker';
+declare module 'react-slider';
+declare module '@radix-ui/react-dialog';
+declare module '@radix-ui/react-select';
+declare module '@radix-ui/react-slider';
+declare module '@radix-ui/react-tabs';
+declare module 'tailwind-merge';
